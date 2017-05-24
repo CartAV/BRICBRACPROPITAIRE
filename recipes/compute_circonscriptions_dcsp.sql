22f0ef102a3a1aafc8acaf1b3ecd78468659fb6d
grabenertr <grabenertr>
2017-05-24 15:19:57 +0200

    Saved recipe: BRICBRACPROPITAIRE.compute_circonscriptions_dcsp

--- recipes/compute_circonscriptions_dcsp.sql
+++ recipes/compute_circonscriptions_dcsp.sql
@@ -1,4 +1,6 @@
-SELECT "nom_circo", st_multi(st_union("the_geom"::geometry)) as geom
+SELECT "nom_circo",
+       st_multi(st_union("the_geom"::geometry)) as geom,
+       st_geojson(st_multi(st_union("the_geom"::geometry))) as geojson
   FROM "liste_communes_police_prepared", ign_commune_france
   WHERE "n°_commune_insee_long" = "INSEE_COM"
   GROUP BY "nom_circo"
