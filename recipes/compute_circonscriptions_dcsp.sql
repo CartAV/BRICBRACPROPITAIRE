eb86b11f013c8eec974af371ae54db8d5ba63389
grabenertr <grabenertr>
2017-05-24 14:24:34 +0200

    Saved recipe: BRICBRACPROPITAIRE.compute_circonscriptions_dcsp

--- recipes/compute_circonscriptions_dcsp.sql
+++ recipes/compute_circonscriptions_dcsp.sql
@@ -1,4 +1,4 @@
 SELECT "nom_circo", st_union("the_geom")
-  FROM "liste_communes_police_prepared", "IGN_COMMUNE_FRANCE"
+  FROM "liste_communes_police_prepared", ign_commune_france
   WHERE "n°_commune_insee_long" = "INSEE_COM"
   GROUP BY "nom_circo"
