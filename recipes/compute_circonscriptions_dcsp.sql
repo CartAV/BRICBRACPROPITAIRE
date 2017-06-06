1ae7eddc3e2168da4cf6466b397ff6bc5195e714
chabouisfr <chabouisfr>
2017-06-06 16:55:37 +0200

    Saved recipe: BRICBRACPROPITAIRE.compute_circonscriptions_dcsp

--- recipes/compute_circonscriptions_dcsp.sql
+++ recipes/compute_circonscriptions_dcsp.sql
@@ -1,7 +1,7 @@
 SELECT "nom_circo",
        st_multi(st_union("the_geom"::geometry)) as geom,
        st_asgeojson(st_multi(st_union("the_geom"::geometry))) as geojson,
-       st_aswkt(st_multi(st_union("the_geom"::geometry))) as WKT
+       st_astext(st_multi(st_union("the_geom"::geometry))) as WKT
   FROM "liste_communes_police_prepared", ign_commune_france
   WHERE "n°_commune_insee_long" = "INSEE_COM"
   GROUP BY "nom_circo"
