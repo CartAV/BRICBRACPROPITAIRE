7f23b20a30435776c710e4b95bc6234d0c6b0edb
grabenertr <grabenertr>
2017-10-10 11:34:21 +0200

    Deleted recipe: BRICBRACPROPITAIRE.compute_circonscriptions_dcsp

--- recipes/compute_circonscriptions_dcsp.sql
+++
@@ -1,7 +0,0 @@
-SELECT "nom_circo",
-       st_multi(st_union("the_geom"::geometry)) as geom,
-       st_asgeojson(st_multi(st_union("the_geom"::geometry))) as geojson,
-       st_astext(st_multi(st_union("the_geom"::geometry))) as WKT
-  FROM "liste_communes_police_prepared", ign_commune_france
-  WHERE "n°_commune_insee_long" = "INSEE_COM"
-  GROUP BY "nom_circo"
